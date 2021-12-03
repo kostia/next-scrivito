85c9f6dd5ca6a203cb182bb8aa38a4af55a38c31
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2021-12-03 13:09:46 +0100

    Display a scrivito link (+ config)

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,13 @@ import { useEffect } from 'react';
 import * as Scrivito from 'scrivito';
 import styles from '../styles/Home.module.css'
 
-Scrivito.configure({ tenant: "0123456789abcdef0123456789abcdef" });
+Scrivito.configure({ tenant: "248dcdc062ec51d2f89f91c23d192898" });
 
 const Home: NextPage = () => {
   useEffect(() => {
     Scrivito.load(() => Scrivito.Obj.getByPath('/')).then(obj => console.log(obj));
   });
+
   return (
     <div className={styles.container}>
       <Head>
@@ -20,58 +21,10 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Scrivito.LinkTag to={ new Scrivito.Link({url: 'https://scrivito.com'})}>
+          scrivito.com
+        </Scrivito.LinkTag>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
